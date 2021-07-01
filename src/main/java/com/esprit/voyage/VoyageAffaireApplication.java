b5f8c555d511c2bf4eb28618961fc2b30f19dd9d
wiem.benmohamed@esprit.tn <wiem.benmohamed@esprit.tn>
2021-07-01 21:14:42 +0100

    first commit

---
+++ src/main/java/com/esprit/voyage/VoyageAffaireApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.voyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageAffaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoyageAffaireApplication.class, args);
+	}
+
+}
